e67c7deb96616bf5b4bcb440c4e96ea1de9b5f07
Carmen Alvarez <c@rmen.ca>
2018-10-03 12:27:22 +0200

    Initial Commit

---
+++ PoetAssistant/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  PoetAssistant
+//
+//  Created by Carmen Alvarez on 03/10/2018.
+//  Copyright © 2018 Carmen Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
