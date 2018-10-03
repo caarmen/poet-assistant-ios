023298195162bb44c15a0daf288ccd914628c6b4
Carmen Alvarez <c@rmen.ca>
2018-10-03 16:10:35 +0200

    Start implementing the reader

---
+++ PoetAssistant/src/composer/ComposerViewController.swift
@@ -0,0 +1,47 @@
+//
+//  ComposerViewController.swift
+//  PoetAssistant
+//
+//  Created by Carmen Alvarez on 03/10/2018.
+//  Copyright © 2018 Carmen Alvarez. All rights reserved.
+//
+
+import UIKit
+
+class ComposerViewController: UIViewController, UITextViewDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+		updateTextHint()
+    }
+    
+	@IBOutlet weak var text: UITextView! {
+		didSet {
+			text.delegate = self
+		}
+	}
+	
+	@IBOutlet weak var hint: UILabel!
+	@IBAction func onShare(_ sender: Any) {
+		present(UIActivityViewController(activityItems: [text.text], applicationActivities: nil), animated:true, completion:nil)
+	}
+	
+	private func updateTextHint() {
+		hint.isHidden = !text.text.isEmpty
+	}
+	
+	func textViewDidChange(_ textView: UITextView) {
+		updateTextHint()
+	}
+
+	/*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
