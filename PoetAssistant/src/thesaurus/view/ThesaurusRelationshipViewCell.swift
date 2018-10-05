912136541b707e68eb0b6b16acb8b560c32d6972
Carmen Alvarez <c@rmen.ca>
2018-10-05 22:11:39 +0200

    Added license

--- PoetAssistant/src/thesaurus/view/ThesaurusRelationshipViewCell.swift
+++ PoetAssistant/src/thesaurus/view/ThesaurusRelationshipViewCell.swift
@@ -1,10 +1,21 @@
-//
-//  ThesaurusRelationshipViewCell.swift
-//  PoetAssistant
-//
-//  Created by Carmen Alvarez on 05/10/2018.
-//  Copyright © 2018 Carmen Alvarez. All rights reserved.
-//
+/**
+ Copyright (c) 2018 Carmen Alvarez
+
+ This file is part of Poet Assistant.
+
+ Poet Assistant is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Poet Assistant is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Poet Assistant.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 import UIKit
 
