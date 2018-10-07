dc5d771bb969ef962a846a055815594f97ef86c2
Carmen Alvarez <c@rmen.ca>
2018-10-07 12:07:21 +0200

    Issue #2: Exclude redundant rhymes.

--- PoetAssistant/src/rhymer/model/WordVariants.swift
+++ PoetAssistant/src/rhymer/model/WordVariants.swift
@@ -1,20 +1,20 @@
 /**
- Copyright (c) 2018 Carmen Alvarez
+Copyright (c) 2018 Carmen Alvarez
 
- This file is part of Poet Assistant.
+This file is part of Poet Assistant.
 
- Poet Assistant is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
+Poet Assistant is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
 
- Poet Assistant is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
+Poet Assistant is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
 
- You should have received a copy of the GNU General Public License
- along with Poet Assistant.  If not, see <http://www.gnu.org/licenses/>.
+You should have received a copy of the GNU General Public License
+along with Poet Assistant.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 import CoreData
@@ -31,36 +31,113 @@ class WordVariants: NSManagedObject {
 		if let wordVariants = try? request.execute() {
 			for (index, wordVariant) in wordVariants.enumerated() {
 				let variantPrefix = (wordVariants.count > 1) ? "\(index + 1)." : ""
-				if let stress_syllables = wordVariant.stress_syllables {
-					result.add(sectionTitle: "\(variantPrefix)0", fetchedResultsController: createFetchResultsControllerForRhymeType(context: context, queryWord: queryText, rhymeTypeColumn: #keyPath(WordVariants.stress_syllables), rhymeValue: stress_syllables))
+				result.add(
+					sectionTitle: "\(variantPrefix)0",
+					fetchedResultsController: createFetchResultsControllerForRhymeType(
+						context: context,
+						predicate: createPredicateForStressSyllableMatch(wordVariant: wordVariant)))
+				if wordVariant.last_three_syllables != nil {
+					result.add(
+						sectionTitle: "\(variantPrefix)3",
+						fetchedResultsController: createFetchResultsControllerForRhymeType(
+							context: context,
+							predicate: createPredicateForLastThreeSyllableMatch(wordVariant: wordVariant)!))
 				}
-				if let last_three_syllables = wordVariant.last_three_syllables {
-					result.add(sectionTitle: "\(variantPrefix)3", fetchedResultsController: createFetchResultsControllerForRhymeType(context: context, queryWord: queryText, rhymeTypeColumn: #keyPath(WordVariants.last_three_syllables), rhymeValue: last_three_syllables))
+				if wordVariant.last_two_syllables != nil {
+					result.add(
+						sectionTitle: "\(variantPrefix)2",
+						fetchedResultsController: createFetchResultsControllerForRhymeType(
+							context: context,
+							predicate: createPredicateForLastTwoSyllableMatch(wordVariant: wordVariant)!))
 				}
-				if let last_two_syllables = wordVariant.last_two_syllables {
-					result.add(sectionTitle: "\(variantPrefix)2", fetchedResultsController: createFetchResultsControllerForRhymeType(context: context, queryWord: queryText, rhymeTypeColumn: #keyPath(WordVariants.last_two_syllables), rhymeValue: last_two_syllables))
-				}
-				if let last_syllable = wordVariant.last_syllable {
-					result.add(sectionTitle: "\(variantPrefix)1", fetchedResultsController: createFetchResultsControllerForRhymeType(context: context, queryWord: queryText, rhymeTypeColumn: #keyPath(WordVariants.last_syllable), rhymeValue: last_syllable))
-				}
+				result.add(
+					sectionTitle: "\(variantPrefix)1",
+					fetchedResultsController: createFetchResultsControllerForRhymeType(
+						context: context,
+						predicate: createPredicateForLastSyllableMatch(wordVariant: wordVariant)))
 			}
 		}
 		
 		return result
 	}
 	
+	private class func createPredicateForStressSyllableMatch(wordVariant: WordVariants) -> NSPredicate {
+		let matchingPredicate = NSPredicate(format: "\(#keyPath(WordVariants.stress_syllables)) =[c] %@", wordVariant.stress_syllables!) // TODO update schema to make stress_syllables not optional
+		let excludingPredicate = createPredicateExcludingSameWord(wordVariant: wordVariant)
+		return concatenatePredicates(subpredicates: [matchingPredicate, excludingPredicate])
+	}
+	
+	private class func createPredicateForLastThreeSyllableMatch(wordVariant: WordVariants) -> NSPredicate? {
+		if wordVariant.last_three_syllables == nil {
+			return nil
+		}
+		let matchingPredicate = NSPredicate(format: "\(#keyPath(WordVariants.last_three_syllables)) =[c] %@", wordVariant.last_three_syllables!)
+		let excludeSameWordPredicate = createPredicateExcludingSameWord(wordVariant: wordVariant)
+		let excludeStressSyllablesPredicate = createPredicateExcludingStressSyllables(wordVariant: wordVariant)
+		return concatenatePredicates(subpredicates: [matchingPredicate, excludeSameWordPredicate, excludeStressSyllablesPredicate])
+	}
+	
+	private class func createPredicateForLastTwoSyllableMatch(wordVariant: WordVariants) -> NSPredicate? {
+		if wordVariant.last_two_syllables == nil {
+			return nil
+		}
+		let matchingPredicate = NSPredicate(format: "\(#keyPath(WordVariants.last_two_syllables)) =[c] %@", wordVariant.last_two_syllables!)
+		let excludeSameWordPredicate = createPredicateExcludingSameWord(wordVariant: wordVariant)
+		let excludeStressSyllablesPredicate = createPredicateExcludingStressSyllables(wordVariant: wordVariant)
+		let excludeLastThreeSyllablesPredicate = createPredicateExcludingLastThreeSyllables(wordVariant: wordVariant)
+		return concatenatePredicates(subpredicates: [matchingPredicate, excludeSameWordPredicate, excludeStressSyllablesPredicate, excludeLastThreeSyllablesPredicate])
+	}
+	
+	private class func createPredicateForLastSyllableMatch(wordVariant: WordVariants) -> NSPredicate {
+		let matchingPredicate = NSPredicate(format: "\(#keyPath(WordVariants.last_syllable)) =[c] %@", wordVariant.last_syllable!)
+		let excludeSameWordPredicate = createPredicateExcludingSameWord(wordVariant: wordVariant)
+		let excludeStressSyllablesPredicate = createPredicateExcludingStressSyllables(wordVariant: wordVariant)
+		let excludeLastThreeSyllablesPredicate = createPredicateExcludingLastThreeSyllables(wordVariant: wordVariant)
+		let excludeLastTwoSyllablesPredicate = createPredicateExcludingLastTwoSyllables(wordVariant: wordVariant)
+		return concatenatePredicates(subpredicates: [matchingPredicate, excludeSameWordPredicate, excludeStressSyllablesPredicate, excludeLastThreeSyllablesPredicate, excludeLastTwoSyllablesPredicate])
+	}
+	
+	private class func concatenatePredicates(subpredicates: [NSPredicate?]) -> NSPredicate {
+		let nonNullPredicates = subpredicates.filter { subpredicate in
+			return subpredicate != nil
+			} as! [NSPredicate]
+		return NSCompoundPredicate(type: .and, subpredicates: nonNullPredicates)
+	}
+	private class func createPredicateExcludingSyllables(syllablesValue: String?, syllablesColumn: String) -> NSPredicate? {
+		if syllablesValue != nil {
+			return NSCompoundPredicate(
+				type: .or,
+				subpredicates: [NSPredicate(format: "\(syllablesColumn) = nil"),
+								NSPredicate(format: "\(syllablesColumn) !=[c] %@", syllablesValue!)])
+		} else {
+			return nil
+		}
+	}
+	
+	private class func createPredicateExcludingSameWord(wordVariant: WordVariants) -> NSPredicate {
+		return NSPredicate(format: "\(#keyPath(WordVariants.word)) !=[c] %@ AND \(#keyPath(WordVariants.has_definition)) = 1",
+			wordVariant.word!) // TODO update schema to make word not optional
+	}
+	private class func createPredicateExcludingStressSyllables(wordVariant: WordVariants) -> NSPredicate? {
+		return createPredicateExcludingSyllables(syllablesValue: wordVariant.stress_syllables, syllablesColumn: #keyPath(WordVariants.stress_syllables))
+	}
+	private class func createPredicateExcludingLastThreeSyllables(wordVariant: WordVariants) -> NSPredicate? {
+		return createPredicateExcludingSyllables(syllablesValue: wordVariant.last_three_syllables, syllablesColumn: #keyPath(WordVariants.last_three_syllables))
+	}
+	private class func createPredicateExcludingLastTwoSyllables(wordVariant: WordVariants) -> NSPredicate? {
+		return createPredicateExcludingSyllables(syllablesValue: wordVariant.last_two_syllables, syllablesColumn: #keyPath(WordVariants.last_two_syllables))
+	}
+	
 	private class func createFetchResultsControllerForRhymeType(
 		context: NSManagedObjectContext,
-		queryWord: String,
-		rhymeTypeColumn: String,
-		rhymeValue: String) -> NSFetchedResultsController<NSDictionary> {
+		predicate: NSPredicate) -> NSFetchedResultsController<NSDictionary> {
 		
 		let request = NSFetchRequest<NSDictionary>(entityName: "WordVariants")
 		request.propertiesToFetch = [#keyPath(WordVariants.word)]
 		request.resultType = .dictionaryResultType
 		request.returnsDistinctResults = true
 		request.sortDescriptors = [NSSortDescriptor(key: #keyPath(WordVariants.word), ascending: true)]
-		request.predicate = NSPredicate(format: "\(rhymeTypeColumn) == %@ AND \(#keyPath(WordVariants.word)) !=[c] %@ AND \(#keyPath(WordVariants.has_definition)) = 1", rhymeValue, queryWord)
+		request.predicate = predicate
 		return NSFetchedResultsController(
 			fetchRequest: request,
 			managedObjectContext: context,
