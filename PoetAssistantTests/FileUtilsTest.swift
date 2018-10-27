694e2ae99da9d15dcaf0e6621ef7f91a323c893b
Carmen Alvarez <c@rmen.ca>
2018-10-28 00:41:29 +0200

    Added tests for filenames.

---
+++ PoetAssistantTests/FileUtilsTest.swift
@@ -0,0 +1,36 @@
+/**
+Copyright (c) 2018 Carmen Alvarez
+
+This file is part of Poet Assistant.
+
+Poet Assistant is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Poet Assistant is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Poet Assistant.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+import XCTest
+@testable import PoetAssistant
+class FileUtilsTest: XCTestCase {
+	
+	func testGetUsableFilename() {
+		testGetUsableFilename(expectedFilename:"YoLo.txt", actualUserInput:"YoLo.txt")
+		testGetUsableFilename(expectedFilename:"hello.txt", actualUserInput:"hello")
+		testGetUsableFilename(expectedFilename:"poem.txt", actualUserInput:"👨🏽‍✈️")
+		testGetUsableFilename(expectedFilename:"somefileswithspaces.txt", actualUserInput:"some files with spaces")
+		testGetUsableFilename(expectedFilename:"slash.txt", actualUserInput:"slash/")
+	}
+	
+	private func testGetUsableFilename(expectedFilename: String, actualUserInput: String) {
+		XCTAssertEqual(expectedFilename, FileUtils.getUsableFilename(userEnteredFilename: actualUserInput))
+	}
+}
