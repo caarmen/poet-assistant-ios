f1827368b3e8543c1d30ef98307ecd35b750ecc0
Carmen Alvarez <c@rmen.ca>
2020-12-21 17:58:08 +0100

    Support pause tokens in tts
    Port the logic from the android implementation:
    * [TtsSplitter](https://github.com/caarmen/poet-assistant/blob/master/app/src/main/kotlin/ca/rmen/android/poetassistant/TtsSplitter.kt)
    * [TestTtsSplitter](https://github.com/caarmen/poet-assistant/blob/master/app/src/test/java/ca/rmen/android/poetassistant/TestTtsSplitter.java)
    
    Treat a  `...` in a text as a half-second pause.  Treat subsequent `.` as additional half-second pauses.

--- PoetAssistant/src/tts/Tts.swift
+++ PoetAssistant/src/tts/Tts.swift
@@ -20,17 +20,80 @@ along with Poet Assistant.  If not, see <http://www.gnu.org/licenses/>.
 import AVFoundation
 
 class Tts {
+	private static let PAUSE_DURATION_S = TimeInterval(0.5)
+
 	class func createUtterance(text: String) -> AVSpeechUtterance {
 		return createUtterance(text:text, voiceIdentifier: Settings.getVoiceIdentifier())
 	}
 	
-	class func createUtterance(text: String, voiceIdentifier: String?) -> AVSpeechUtterance {
+	class func createUtterance(text: String, voiceIdentifier: String?, preDelay: TimeInterval? = nil) -> AVSpeechUtterance {
 		let utterance = AVSpeechUtterance(string: text)
 		utterance.rate = Settings.getVoiceSpeed()
 		utterance.pitchMultiplier = Settings.getVoicePitch()
+		if (preDelay != nil) {
+			utterance.preUtteranceDelay = preDelay!
+		}
 		if voiceIdentifier != nil, let voice = AVSpeechSynthesisVoice(identifier: voiceIdentifier!) {
 			utterance.voice = voice
 		}
 		return utterance
+	}
+
+	/**
+	* Splits a string into multiple utterances for pausing playback.
+	*
+	* @param text A "..." in the input text indicates a pause, and each subsequent "." after the initial "..." indicates an additional pause.
+	*
+	* Examples:
+	* "To be or not to be... that is the question":  1 pause:  "To be or not to be", (pause), " that is the question"
+	* "To be or not to be.... that is the question": 2 pauses: "To be or not to be", (pause), (pause), " that is the question"
+	* "To be or not to be. that is the question":    0 pauses: "To be or not to be. that is the question"
+	*
+	* @return the input split into multiple tokens. An empty-string token in the result indicates a pause.
+	*/
+	class func createUtterances(text: String) -> [AVSpeechUtterance] {
+		let voiceIdentifier = Settings.getVoiceIdentifier()
+		var result = [AVSpeechUtterance]()
+		// In a sequence of dots, we want to skip the first two.
+		var skippedDots = 0
+		var pause = TimeInterval(0)
+		var prevUtterance: AVSpeechUtterance? = nil
+		text.components(separatedBy: ".").forEach { token in
+			// The current token is a dot. It may or may not be used to pause.
+			if (token.isEmpty) {
+				// We've skipped at least two consecutive dots. We can now start adding all dots as
+				// pause tokens.
+				if (skippedDots >= 1) {
+					pause += PAUSE_DURATION_S
+					prevUtterance = nil
+				}
+				// Beginning of a dot sequence. We have to skip the first two dots.
+				else {
+					skippedDots += 1
+				}
+			}
+			// The current token is actual text to speak.
+			else {
+				var utterance: AVSpeechUtterance? = nil
+				// This is either the first text token of the entire input, or a text token after a pause token.
+				// We simply add it to the list.
+				if (prevUtterance == nil || prevUtterance!.speechString.isEmpty) {
+					utterance = createUtterance(text: token, voiceIdentifier: voiceIdentifier, preDelay: pause)
+					result.append(utterance!)
+				}
+				// The previous token was also actual text.
+				// Concatenate the previous token with this one, separating by a single period.
+				// This optimization allows us to minimize the number of tokens we'll return, and to rely
+				// on the sentence pausing of the TTS engine when less than 3 dots separate two sentences.
+				else {
+					utterance = createUtterance(text: "\(prevUtterance!.speechString).\(token)", voiceIdentifier: voiceIdentifier)
+					result[result.count - 1] = utterance!
+				}
+				prevUtterance = utterance
+				pause = 0
+				skippedDots = 0
+			}
+		}
+		return result
 	}
 }
